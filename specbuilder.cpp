ee541f7b11f84be994394331ccf174a306254283
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2022-03-10 13:32:02 +0200

    Spec Builder Class framework

---
+++ specbuilder.cpp
@@ -0,0 +1,32 @@
+#include "specbuilder.h"
+
+specbuilder::specbuilder(dataHolder *db, QObject *parent)
+    : m_db(db),
+      QObject{parent}
+{}
+
+void specbuilder::mining(int budget)
+{
+
+}
+
+void specbuilder::server(int budget)
+{
+
+}
+
+void specbuilder::office(int budget)
+{
+
+}
+
+void specbuilder::overclocking(int budget)
+{
+
+}
+
+void specbuilder::build(int budget, QString purpose)
+{
+
+}
+
