eaef680da58eaf73935dbc22cb8c2101bf44fd59
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2022-03-16 20:48:07 +0200

    Update main.cpp
    Removed unnecessary code

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
-//#include "Test/include/dbtests.h"
-#include "Test/include/specbuildertests.h"
 #include <QCoreApplication>
-#include <QList>
-#include <iostream>
 
-using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    specBuilderTests f("/home/nave/Documents/QtProjects/PCPartPicker/Test");
-    dataHolder h("/home/nave/Documents/QtProjects/PCPartPicker/Test/");
-    f.generate(&h);
-    f.backup2(&h);
     return a.exec();
 }
