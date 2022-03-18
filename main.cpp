d94dc4d2b4950e48f60451d737545a7d5492f634
Nave <68282476+ThatGuyVanquish@users.noreply.github.com>
2022-03-18 13:55:50 +0200

    Update main.cpp
    Finalized main class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <QCoreApplication>
 
+#include "include/Logic/pcpartpicker.h"
+
+using namespace std;
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    pcpartpicker app("/home/nave/Documents/QtProjects/PCPartPicker/database");
+    app.run();
     return a.exec();
 }
